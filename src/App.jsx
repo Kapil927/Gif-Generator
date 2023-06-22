4d618137a9fc2d39be678fd2add8cdcdc95f1c49
Kapil Thakur <kapilcambridge9@gmail.com>
2023-06-22 18:22:16 +0530

    First commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,47 @@
-import Random from "./components/Random"
-import Tag from "./components/Tag";
+import { useContext } from "react";
+import { Context } from "./Context_Provider/ContextProvider";
+import { useEffect } from "react";
+import { useSearchParams, useLocation, Routes, Route } from "react-router-dom";
+import HomePage from "./Pages/HomePage";
+import BlogPage from "./Pages/BlogPage";
+import CategoryPage from "./Pages/CategoryPage";
+import TagPage from "./Pages/TagPage";
 
 export default function App() {
+
+      const {fetchData} = useContext(Context);
+    const[searchParams, setSearchParams] = useSearchParams();
+    const location = useLocation();
+  
+    useEffect(()=>{
+      const page = searchParams.get("page") ?? 1;
+     if(location.pathname.includes("tag")){
+      const tag = location.pathname.split("/").at(-1).replaceAll("_"," ");
+      fetchData(Number(page), tag);
+     }
+                                                        
+     if(location.pathname.includes("categories")){
+      const category = location.pathname.split("/").at(-1).replaceAll("_"," ");
+      fetchData(Number(page), null, category);
+     }
+      
+     else{
+      fetchData(Number(page));
+                                          }
+                  },[location.pathname, location.search]);
+
   return (
 
     <div className="w-full h-screen flex flex-col background relative overflow-x-hidden items-center">
-      <h1 className=" bg-white rounded-lg w-11/12 text-center mt-[40px]
-       px-10 py-2 text-4xl font-bold ">
-      RANDOM GIFS</h1>
       <div className="flex flex-col w-full items-center gap-y-10 mt-[30px]">
-        <Random />
-        <Tag />
+          <Routes>
+    <Route path="/" element={<HomePage/>}/>
+    <Route path="/blogs/:blogId" element={<BlogPage/>}/>
+    <Route path="/categories/:category" element={<CategoryPage/>}/>
+    <Route path="/tags/:tag" element={<TagPage/>}/>
+    <Route path="*" element={<div>Not found</div>}></Route>
+   </Routes> 
       </div>
     </div>
-
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,10 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import { BrowserRouter } from 'react-router-dom';
+import ContextProvider from './Context_Provider/ContextProvider';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+         <BrowserRouter>
+         <ContextProvider>
+                    <App/>
+         </ContextProvider>
+      </BrowserRouter>
   </React.StrictMode>
 );
